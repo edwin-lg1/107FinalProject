dbdf6b77666c51d89a4c5ad3d1fa7508b728e6b5
andychen0131 <73270004+andychen0131@users.noreply.github.com>
2023-12-12 16:48:06 -0500

    Add files via upload

--- ModularFinal.m
+++ ModularFinal.m
@@ -7,16 +7,19 @@ clc
 % EE 107 - Communication Systems - Final Project
 % Authors: Nicholas Boudreau, Andrew Chen, Edwin Liang-Gilman
 
-%%
-% Modularize process
-% 
-% image preprocess
-filename = 'octo256.jpg';
-% imfinfo(filename)
-qbits = 8;
+%% Specify Parameters
+filename = 'octopus.png';
+
+qbits = 8; % [8 16 color depth]
+color = 1; % [0 greyscale, 1 color]
+pulseshape = 1; % [1 HS, 2 SRRC]
+channel = 2; % [1 default, 2 outdoor, 3 indoor]
+noisepwr = 1; % [1 LOW 1e-2, 2 HIGH 1e-1]
+eq = 2; % [1 zf 2 mmse]
+
 %%
 % run
-main(filename,qbits)
+main(filename,qbits,color,pulseshape,channel,eq, noisepwr)
 
 
 
@@ -25,172 +28,239 @@ main(filename,qbits)
 
 
 %% main driver 
-function main(img_filename, qbits)
+function main(img_filename, qbits, color, pulseshape, channel, eq, noisepwr)
     % image preproc
-    [Ztres,r,c,m,n,minval,maxval] = ImagePreProcess_color(img_filename, qbits);
-%     [Ztres_gray,r,c,m,n,minval,maxval]=ImagePreProcess_gray(img_filename,qbits)
+    switch color
+        case 1
+            [Ztres,r,c,m,n,minval,maxval] = ImagePreProcess_color(img_filename, qbits);
+        otherwise
+            img_filename = 'cameraman.tif';
+            [Ztres,r,c,m,n,minval,maxval] = ImagePreProcess_gray(img_filename, qbits);
+    end
+
     img_RGB = imread(img_filename);
-%     img_double = im2double(img_RGB);
     numcol = floor(numel(img_RGB(1,:,1))/2);
 
-    % ->
-    % bit stream
-    % convert size(img_bitstream) to 1-dimensional bit-stream using reshape
+    %%%% Bit stream %%%%
 
-    DCTblocks = int2bit(Ztres,8);
-    stack = zeros(8,8,size(DCTblocks,3));
+    DCTblocks = int2bit(Ztres,8);           % 64x8xNumberBlocks uint8
+    stack = zeros(8,8,size(DCTblocks,3));   % 8x8xNumberBlocks double
+
+    %%%% Constants for processing %%%%
+    samps = 32;
+    Tb = 1;
+    K = 6;
+    alpha = 0.5;
+
+    Eb = 1;
     
-    for z = 1:size(DCTblocks,3) % 4 blocks
+    for z = 1%:size(DCTblocks,3) % r/8*c/8*3 (108)
+        disp(z)
         ak = reshape(DCTblocks(:,:,z),[],1);
         ak_double = cast(ak, 'double');
         bk = 2*ak_double-1;
-%         bk = upsample(bk,32);
+        bk = upsample(bk,32);
         img_bitstream = bk;
         n_bits = size(DCTblocks,1)*size(DCTblocks,2);
-        % ->
-        % modulate with 2 pulse shapes
-%         modulated_sig_cellarray = cell(2,1);
-%         t_cellarray = cell(2,1);
+        hs_vectorlength = n_bits*32;
         
-        Tb = 1;
-        K = 6;
-        alpha = 0.5;
-        samps = 32;
+        switch pulseshape
+            case 1
+%                 disp("HS Modulation")
+                [modulated_sig, pulse, pulse_t, ...
+                    Tb, K, alpha, samps] = pulseshape_modulation(img_bitstream, ...
+                                                                pulseshape, Tb, K, alpha, samps);
+                
+                hs_mod = modulated_sig(1:hs_vectorlength);
+                channel_input_sig = hs_mod;
 
-        for i = 2 % 1 selects Half-Sine
-            [modulated_sig, t, ...
-                pulse, pulse_t, Tb, K, alpha, samps] = pulseshape_modulation(img_bitstream, ...
-                                                             i, Tb, K, alpha, samps);
-        for j = 1%:3 % 1 selects channel 1
-            [noisy_received_sig, clean_channel_sig, taps, npwr] = channel_modulation(modulated_sig, j);
+            case 2
+%                 disp("SRRC Modulation")
+
+                channel_input_sig = [];
+
         end
-        noisy_low = noisy_received_sig(1,:);
-        noisy_high = noisy_received_sig(2,:);
-        noisy_0 = clean_channel_sig(:);
+            %%%%% Channel Modulation %%%%
+        switch channel
+            case 1
+%                 disp("Channel Definition 1")
+                [noisy_received_sig, clean_channel_sig, taps, npwr] = channel_modulation(channel_input_sig, channel, noisepwr);
+            case 2
+%                 disp("Outdoor Channel")
+                [noisy_received_sig, clean_channel_sig, taps, npwr] = channel_modulation(channel_input_sig, channel, noisepwr);
+            case 3
+%                 disp("Indoor Channel")
+                [noisy_received_sig, clean_channel_sig, taps, npwr] = channel_modulation(channel_input_sig, channel, noisepwr);
+        end
 
-        [matchedfiltered_sig, matchedfilter] = matched_filtering(noisy_0,pulse);
+        if pulseshape
+            noisy_received_sig = noisy_received_sig(1:hs_vectorlength);
+        else
+            noisy_received_sig = [];
+
+        end
+            rx_zeronoise = clean_channel_sig;
+            rx_noisy = noisy_received_sig;
+
+            %%%% MATCHED FILTERING %%%%
+
+            [rx_zero_noise_mf, matchedfilter] = matched_filtering(rx_zeronoise,pulse);
+            [rx_noisy_mf, matchedfilter] = matched_filtering(rx_noisy,pulse);
+
+        if pulseshape
+            rx_noisy_mf = rx_noisy_mf(1:hs_vectorlength);
+            rx_zero_noise_mf = rx_zero_noise_mf(1:hs_vectorlength);
+
+        else
+            rx_noisy_mf = [];
+            rx_zero_noise_mf = [];
+        end
+
+
+            %%%% Equalizer %%%%
+            [H,w1] = freqz(taps,1,10000,'whole');
+
+        switch eq
+            case 1
+%                 disp("Zero Forcing Eq")
+                [rx_zeronoise_zf, Q_zf] = zf_equalizer(rx_zero_noise_mf,H);
+                [rx_noisy_zf, Q_zf] = zf_equalizer(rx_noisy_mf,H);
+
+
+            case 2
+%                 disp("MMSE Eq")
+                [rx_zeronoise_mmse,Q_mmse] = mmse(rx_zero_noise_mf,H,0,Eb);
+                [rx_noisy_mmse,Q_mmse] = mmse(rx_noisy_mf,H,npwr,Eb);
+
+        end
+
+
+        if pulseshape
+%             rx_zeronoise_zf = rx_zeronoise_zf(1:hs_vectorlength);
+%             rx_noisy_zf = rx_noisy_zf(1:hs_vectorlength);
+            rx_zeronoise_mmse = rx_zeronoise_mmse(1:hs_vectorlength);
+            rx_noisy_mmse = rx_noisy_mmse(1:hs_vectorlength);
+
+        else
+%             rx_zeronoise_zf = [];
+%             rx_noisy_zf = [];
+            rx_zeronoise_mmse =[];
+            rx_noisy_mmse =[];
+        end
+
         
-        %EQ
-        zero_forced_sig = filter(1,taps,matchedfiltered_sig);
-        H = freqz(taps,1);
-        noise_pow = 0;
-        Eb = 1;
-        [mmse_sig,Q_mmse] = mmse(matchedfiltered_sig',H,noise_pow,Eb);
+               
+            bitstream_sampled = zeros(n_bits,1);
+        %%%% Sampling and Detection %%%%
 
-        bitstream_sampled = zeros(n_bits,1);
- 
-        bitstream_sampled(zero_forced_sig((161:samps:end-1>0))) = 1;
+        % HS
+        if pulseshape
+%             disp("HS Sampling and Detection")
+            bitstream_sampled(rx_zeronoise_mmse(32:32:end)>0) = 1;
 
-        ak_reconstructed = cast(bitstream_sampled,'uint8');
-        ak_bin = reshape(ak_reconstructed,64,8);
-        ak_recon = bit2int(ak_bin,8);
+        else
+%             bitstream_sampled = [];
+        end
+
+
+%             bitstream_sampled(zero_forced_sig((161:samps:end-1>0))) = 1;
+    
+            ak_reconstructed = cast(bitstream_sampled,'uint8');
+            ak_bin = reshape(ak_reconstructed,64,8);
+            ak_recon = bit2int(ak_bin,8);
+            
+            
+            stack(:,:,z) = ak_recon;
+            
+%             Ztres(:,:,z) == stack(:,:,z)
+
         
+        figure,
+%         plot(clean_channel_sig(1:2000))
+        hold on
+%         plot(noisy_received_sig(1:2000))
+%         plot(channel_input_sig(1:2000))
+        plot(rx_noisy_mmse(1:2000),'LineWidth',1.5)
+        legend()
+        grid minor
         
-        stack(:,:,z) = ak_recon;
-        orig(:,:,z) = Ztres(:,:,1);
-        
-        
-
-%         [newZ]=ImagePostProcess_color(orig,r,c,m,n,minval,maxval);
-
-
 %         figure,
-%         stem(ak_reconstructed)
+%         plot(rx_noisy_zf)
 %         hold on
-%         stem(2*ak_double-1)
-
-
-
-%         [ZF_ht, ZF_t, ZF_f, ZF_w] = ZFeq(taps);
-        
-%         figure,
-%         plot(modulated_sig)
-%         hold on
-%         plot(abs(mmse_sig))
+%         plot(rx_zeronoise_zf)
 %         grid minor
-%         legend()
-%         plot(matchedfiltered_sig)
-%         plot(zero_forced_sig)
-
-
-
-% 
-%         figure(99)
-%         plot(noisy_received_sig(1,1:10))
-%         hold on
-%         plot(noisy_received_sig(2,1:10))
-%         plot(clean_channel_sig(1:10), 'LineWidth',2)
-
-
-%     % plot modulated signal
-%             figure(i),
-%             plot(t,modulated_sig)
-      % plot pulse shapes
-%             figure(i+2)
-%             plot(pulse_t,pulse)
-        
-        
-        end
-        
-
-
-
-    % ->
-    % convolve with 3 channels
-
-    % ->
-    % add 3 possible noise levels [0, 0.1, 0.001]
-    
-    % ->
-    % equalize with 2 equalizers
-    
-    % -> 
-%     % sample and detect
-%     bk_reconstructed = img_bitstream; %%%temporary testing
-
-
-
     end
+    %%%% Plot mmse/zf output %%%%
+
+%     figure
+%     subplot(2,1,1)
+%     plot(rx_zeronoise_zf)
+%     title("Rx No Noise ZF")
+%     
+%     subplot(2,1,2)
+%     plot(rx_noisy_zf)
+%     title("Rx Noise:"+npwr+" ZF")
+
+%     subplot(2,2,3)
+%     plot(rx_zeronoise_mmse)
+%     title("Rx No Noise MMSE")
+% 
+% 
+%     subplot(2,2,4)
+%     plot(rx_noisy_mmse)
+%     title("Rx Noise:"+npwr+" MMSE")
 
     [newZ]=ImagePostProcess_color(stack,r,c,m,n,minval,maxval);
-    
-   
-%     % ->
-%     % reshape bitstream into 8x8 for postprocessing
-%     img_reshaped_stream = reshape(img_bitstream_received, 8, 8, []);
-%     % ->
-%     % image postprocess
-%     [img_out] = ImagePostProcess_color(img_reshaped_stream,r,c,m,n,minval,maxval);
-% 
-%     % postproc figures
-%     montage_gen(img_RGB, img_out, numcol)
+    ImagePostProcess_color(Ztres,r,c,m,n,minval,maxval)
 end
 %% Supporting Local Functions
 %
 
 %
-% %
-% function []
-% 
-% MMSE Eq
-function [mmse_sig,Q_mmse] = mmse(g_rec,H,noise_pow,Eb)
+% Zero Forcing Equalizer
+function [zf_sig, Q_zf] = zf_equalizer(rx,H)
+    Q_zf = 1./H;
+    Q_zf = Q_zf';
+
+    zf_sig = eq_sig(rx,Q_zf);
+end
+
+% MMSE Equalizer
+function [mmse_sig,Q_mmse] = mmse(rx,H,noise_pow,Eb)
     H_conj = conj(H);
     H_sq = abs(H).^2;
     Q_mmse = H_conj ./ (H_sq + (noise_pow/Eb));
     Q_mmse = Q_mmse';
 
-    % Apply MMSE Eq.
-    G = fft(g_rec,length(Q_mmse));
-    G_mmse = G .* Q_mmse;
-    mmse_sig = ifft(G_mmse);
-    % check length
+    mmse_sig = eq_sig(rx, Q_mmse);
 end
-%
-% zero-forcing equalizer
-function [ZF_ht, ZF_t, ZF_f, ZF_w] = ZFeq(channel_coeffs)
-    [ZF_f, ZF_w] = freqz(1,channel_coeffs,10000);
-    [ZF_ht, ZF_t] = impz(1,channel_coeffs,2048);
+
+%Equalizer Convolution
+function [eq_sig] = eq_sig(rx,Q_eq)
+    g = fft(rx, length(Q_eq));
+    g_eq = g .* Q_eq;
+    eq_sig = ifft(g_eq);
 end
+% 
+% % MMSE Eq
+% function [mmse_sig,Q_mmse] = mmse(g_rec,H,noise_pow,Eb)
+%     H_conj = conj(H);
+%     H_sq = abs(H).^2;
+%     Q_mmse = H_conj ./ (H_sq + (noise_pow/Eb));
+%     Q_mmse = Q_mmse';
+% 
+%     % Apply MMSE Eq.
+%     G = fft(g_rec,length(Q_mmse));
+%     G_mmse = G .* Q_mmse;
+%     mmse_sig = ifft(G_mmse);
+%     % check length
+% end
+% %
+% % zero-forcing equalizer
+% function [ZF_ht, ZF_t, ZF_f, ZF_w] = ZFeq(channel_coeffs)
+%     [ZF_f, ZF_w] = freqz(1,channel_coeffs,10000);
+%     [ZF_ht, ZF_t] = impz(1,channel_coeffs,2048);
+% end
 
 %
 % matched filtering, provide pulse shape
@@ -198,82 +268,69 @@ function [matchedfiltered_sig, matchedfilter] = matched_filtering(sig,pulse)
     matchedfilter = flip(pulse(1:end-1));%fliplr(pulse);
     matchedfiltered_sig = conv(sig,matchedfilter);
 end
-%
+
 % channel modulation with choice of 3 defined channels
 % returns noisy and clean channel responses, channel coefficients, and
 % noise powers
-function [noisy_received_sig, clean_channel_sig, filter_coeffs, noisepwr] = channel_modulation(sig, channelselect)
-    % channel definition
+function [noisy_received_sig, clean_channel_sig, channel_coeffs, noisepwr] = channel_modulation(sig, channelselect,noisesel)
     ch1 = [1, 0.5, 0.75, -2/7];
     ch_outdoor = [0.5 1 0 0.63 0 0 0 0 0.25 0 0 0 0.16 0 0 0 0 0 0 0 0 0 0 0 0 0.1];
     ch_indoor = [1 0.4365 0.1905 0.0832 0 0.0158 0 0.003];
 
-    noisepwr = [1e-2, 1e-1]';
-    
-
-
+    noisepwr_vector = [1e-2, 1e-1]';
+   
     switch channelselect
         case 1
-            filter_coeffs = ch1;
+            channel_coeffs = ch1;
         case 2
-            filter_coeffs = ch_outdoor;
+            channel_coeffs = ch_outdoor;
         case 3
-            filter_coeffs = ch_indoor;
+            channel_coeffs = ch_indoor;
         otherwise
-            filter_coeffs = [];
+            channel_coeffs = [];
     end
-        upsampled_filter_coeffs = upsample(filter_coeffs,32);
+        upsampled_filter_coeffs = upsample(channel_coeffs,32);
         clean_channel_sig = conv(upsampled_filter_coeffs,sig);
-        n = noisepwr.*randn(size(clean_channel_sig));
-        noisy_received_sig = clean_channel_sig + n;
+
+    switch noisesel
+        case 1
+%             disp("Low Noise Power = 1e-2")
+            noisepwr = noisepwr_vector(noisesel);
+            n = noisepwr.*randn(size(clean_channel_sig));
+            noisy_received_sig = clean_channel_sig + n;
+            
+        case 2
+%             disp("High Noise Power = 1e-1")
+            noisepwr = noisepwr_vector(noisesel);
+            n = noisepwr.*randn(size(clean_channel_sig));
+            noisy_received_sig = clean_channel_sig + n;
+    end
 end
-
-
 
 % modulation with either half-sine or srrc pulses
 % returns modulated signal, pulse signal w.r.t. time, and parameters used
-function [modulated_sig, t, pulse, t_pulse, Tb, K, alpha, samps] = pulseshape_modulation(sig, pulseshape, Tb, K, alpha, samps)
+function [modulated_sig, pulse, t_pulse, Tb, K, alpha, samps] = pulseshape_modulation(sig, pulseshape, Tb, K, alpha, samps)
     modulated_sig = [];
     
     switch pulseshape
         case 1 % half-sine
-%             modulated_sig = zeros() % how to determine sizing
-            % definition
             t_pulse = linspace(0,1,samps+1);
             pulse = sin((pi/Tb)*t_pulse);
             pulse = pulse/norm(pulse);
-            % modulation
-            for j = 1:length(sig)-1
-                modulated_sig = [modulated_sig sig(j)*pulse(1:samps)];
-            end
-            modulated_sig = [modulated_sig sig(length(sig))*pulse];
-            t = linspace(0,length(sig),samps*length(sig)+1);
+            pulse = pulse(1:32);
+
+            modulated_sig = conv(sig,pulse);
 
         case 2 % srrc
             t_pulse = linspace(-K*Tb,K*Tb,2*K*samps+1);
             pulse = rcosdesign(alpha, 2*K, samps);
             pulse = pulse/norm(pulse);
+            pulse = pulse(1:end-1);     
 
-            srrc_ind = [];
-            idx0 = 1;
-            idx1 = 2*K*samps+1;
-            for j = 1:length(sig)
-                srrc_ind(j,idx0:idx1) = sig(j)*pulse;
-                idx0 = idx0 + samps;
-                idx1 = idx1 + samps;
-            end
-
-            for k = 1:length(srrc_ind)
-                modulated_sig = [modulated_sig sum(srrc_ind(:,k))];
-            end
-            t = linspace(0,2*length(sig)+1,length(modulated_sig));
+            modulated_sig = conv(sig,pulse);
 
     end
 end
-% % 
-% function [srrc_modulated, Tb, samps,]
-% 
-% end
 
 % Generate side-by-side image of preprocessed/postprocessed input image
 % Note the artifacts and crush in reduced color depth when transforming
